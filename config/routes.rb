4ef5ea2c2ee5e1c67771a53be2e382e413f968eb
Chris Carlson <chriswc10@chriscasmacbook.pk5001z>
2014-11-26 23:05:16 -0700

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+
+  root 'static#home'
+
+  get '/about', to:'static#about', as: :about
+
+  get 'static/about'
+
+  resources :posts
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
